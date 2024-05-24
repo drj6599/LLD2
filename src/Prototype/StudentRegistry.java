8d63d34118fd7fca779aad24328801303b5931db
drj6599 <drj6599@gmail.com>
2024-05-24 14:32:02 +0530

    Prototype and Registry Design Pattern

---
+++ src/Prototype/StudentRegistry.java
@@ -0,0 +1,24 @@
+package Prototype;
+
+import java.util.HashMap;
+
+public class StudentRegistry {
+    private HashMap<String, Student> studentRegistry;
+
+    public StudentRegistry() {
+        this.studentRegistry = new HashMap<>();
+    }
+
+    public Student add(Student studentPrototype){
+        studentRegistry.put(studentPrototype.getBatchName(), studentPrototype);
+        return studentPrototype;
+    }
+
+    public Student get(String batchName){
+        return studentRegistry.get(batchName);
+    }
+
+    public void remove(String batchName){
+        studentRegistry.remove(batchName);
+    }
+}
